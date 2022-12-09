262a20a18b5d6cbfa214e4ff0beacc83856cea84
usss <88474883+sukimumuu@users.noreply.github.com>
2022-12-09 08:43:00 +0700

    landing page

---
+++ GariMangan/resources/views/landingpage/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/lp.css">
+    <title>GariMangan</title>
+</head>
+<body>
+       <section id="header">
+            <nav>
+                <div class="brand">GariMangan</div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Shop</a></li>
+                <li><a href="">Promo</a></li>
+                <li><a href="">Gallery</a></li>
+                <li><a href=""><button id="login">Login</button></a></li>
+            </ul>
+        </nav>
+       </section>
+       <section id="body">
+              <div class="main-img">
+
+              </div>
+              <div class="promo-today">
+                <p>PROMO TODAY</p>
+                <div class="card-promo">
+                     <div class="card"></div>
+                     <div class="card"></div>
+                     <div class="card"></div>
+                </div>
+              </div>
+       </section>
+
+</body>
+</html>
